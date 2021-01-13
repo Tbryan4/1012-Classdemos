73e9f384bdac4a73fb5f3d3bcb421f1ad8aa0ef0
Tbryan4 <Tbryan4@nait.ca>
2021-01-13 10:28:01 -0700

    start

---
+++ First Sample Solution/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace First_Sample_Solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
