f48a718b0e030eb709ce8096c2755dc98cbdd3d8
Tbryan4 <Tbryan4@nait.ca>
2021-01-13 10:58:18 -0700

    final changes

--- First Sample Solution/Program.cs
+++ First Sample Solution/Program.cs
@@ -6,6 +6,7 @@ namespace First_Sample_Solution
     {
         static void Main(string[] args)
         {
+            // This is the steps on how to withdraw cash from an ATMConsole.WriteLine("Withdrawing Cash From ATM");
             Console.WriteLine("Withdrawing Cash From ATM");
             Console.WriteLine("Step 1: \t Insert a Debit Card into the ATM's Debit Card Slot");
             Console.WriteLine("Step 2: \t Then you would enter the Pin Number from entering pin onto the ATMS pinpad");
@@ -13,7 +14,7 @@ namespace First_Sample_Solution
             Console.WriteLine("Step 4: \t Then you click on the withdraw button, and then withdraw whatever amount I wanted to take");
             Console.WriteLine("Step 5: \t Then the Cash dispenses from the cash slot and then the atm ask if you would like a reciept");
             Console.WriteLine("Step 6: \t Then you would need to grab the cash from the cash slot and take the reciept from the reciept slot and then you would take your card from the card slot");
-            // This is the steps on how to withdraw cash from an ATMConsole.WriteLine("Withdrawing Cash From ATM");
+            
 
             string myName = "Tylen Bryan";
             Console.WriteLine($"This name is printed from a variable is:\t{myName}");
