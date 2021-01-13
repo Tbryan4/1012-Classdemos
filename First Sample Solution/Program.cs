9ddcf50cb2694c92b3316bcee0d09afb7cd70cdf
Tbryan4 <Tbryan4@nait.ca>
2021-01-13 10:42:43 -0700

    commit and code written

--- First Sample Solution/Program.cs
+++ First Sample Solution/Program.cs
@@ -6,7 +6,17 @@ namespace First_Sample_Solution
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Withdrawing Cash From ATM");
+            Console.WriteLine("Step 1: \t Insert a Debit Card into the ATM's Debit Card Slot");
+            Console.WriteLine("Step 2: \t Then you would enter the Pin Number from entering pin onto the ATMS pinpad");
+            Console.WriteLine("Step 3: \t Then you click which account you would like to interact with. In this case its the chequing account");
+            Console.WriteLine("Step 4: \t Then you click on the withdraw button, and then withdraw whatever amount I wanted to take");
+            Console.WriteLine("Step 5: \t Then the Cash dispenses from the cash slot and then the atm ask if you would like a reciept");
+            Console.WriteLine("Step 6: \t Then you would need to grab the cash from the cash slot and take the reciept from the reciept slot and then you would take your card from the card slot");
+            // This is the steps on how to withdraw cash from an ATMConsole.WriteLine("Withdrawing Cash From ATM");
+
+            string myName = "Tylen Bryan";
+            Console.WriteLine($"This name is printed from a variable is:\t{myName}");
         }
     }
 }
